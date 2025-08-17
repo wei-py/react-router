cb92e43e83f094c49245e2b0970ffda678171796
wei-py <rabbwei@outlook.com>
2025-08-18 00:12:51 +0800

    - ✨ Refactor code: Refactor App copy.tsx - ⚠️ Fix(server.ts): 将端口变量从小写port改为大写PORT
    Changes explained:
    - ✨ Refactor code: Refactored the App copy.tsx file to improve maintainability and integration with Vite and React.
    - ⚠️ Fix(server.ts): Fixed the port configuration in server.ts to use the uppercase PORT variable.

--- src/App copy.tsx
+++ src/App copy.tsx
@@ -1,38 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-import viteLogo from "/vite.svg";
+// import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+// import App from "./app";
+// import About from '../about'
+const root = document.getElementById("root");
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount(count => count + 1)}>
-          count is
-          {count}
-        </button>
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
-}
-
-export default App;
+ReactDOM.createRoot(root!).render(
+  <BrowserRouter>
+    <Routes>
+      {/* <Route path="/" element={<App />} /> */}
+      {/* <Route path="about" element={<About />} /> */}
+    </Routes>
+  </BrowserRouter>,
+);
